dff22762b726d4c22899741184c8ef335e9a7d16
AluHnor <uchihamagatsu@hotmail.com>
2025-03-29 03:29:41 -0300

    atualizacao geral

---
+++ java-basico/src/edu/operadores/Operadores.java
@@ -0,0 +1,44 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+    String contatenacao = "?";
+
+    contatenacao = 1+1+1+"1";
+
+    System.out.println(contatenacao);
+
+    contatenacao = 1+"1"+1+1;
+
+    System.out.println(contatenacao);
+
+    contatenacao = "1"+1+1+1;
+
+    System.out.println(contatenacao);
+
+    contatenacao = 1+"1"+1+"1";
+
+    System.out.println(contatenacao);
+
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        System.out.println(- numero);
+
+        // numero = numero * -1;
+
+        // System.out.println(numero);
+
+        numero = numero + 1;
+
+        System.out.println(numero);
+
+        numero++;
+
+        System.out.println(numero);
+
+    }
+}
+
---
+++ java-basico/src/edu/tiposevariaveis/TiposEVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposEVariaveis {
+    public static void main(String[] args)  {
+
+//tipos primitivos
+//classe String representando texto
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 =  (short) numeroNormal;
+
+        int numero = 5;
+            numero =10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+
+
+    }
+}
