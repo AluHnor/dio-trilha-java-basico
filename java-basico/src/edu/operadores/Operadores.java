5fff4e5860c27766b61158d6a237f1e63998ac9c
AluHnor <uchihamagatsu@hotmail.com>
2025-03-31 12:12:15 -0300

    encerramento operadores

--- java-basico/src/edu/operadores/Operadores.java
+++ java-basico/src/edu/operadores/Operadores.java
@@ -1,43 +1,114 @@
 public class Operadores {
     public static void main(String[] args) {
         
-    String contatenacao = "?";
+    // String contatenacao = "?";
 
-    contatenacao = 1+1+1+"1";
+    // contatenacao = 1+1+1+"1";
 
-    System.out.println(contatenacao);
+    // System.out.println(contatenacao);
 
-    contatenacao = 1+"1"+1+1;
+    // contatenacao = 1+"1"+1+1;
 
-    System.out.println(contatenacao);
+    // System.out.println(contatenacao);
 
-    contatenacao = "1"+1+1+1;
+    // contatenacao = "1"+1+1+1;
 
-    System.out.println(contatenacao);
+    // System.out.println(contatenacao);
 
-    contatenacao = 1+"1"+1+"1";
+    // contatenacao = 1+"1"+1+"1";
 
-    System.out.println(contatenacao);
+    // System.out.println(contatenacao);
 
-        int numero = 5;
+        // int numero = 5;
 
-        numero = - numero;
+        // numero = - numero;
 
-        System.out.println(numero);
+        // System.out.println(numero);
 
-        System.out.println(- numero);
+        // System.out.println(- numero);
 
         // numero = numero * -1;
 
         // System.out.println(numero);
 
-        numero = numero + 1;
+        // numero = numero + 1;
 
-        System.out.println(numero);
+        // System.out.println(numero);
 
-        numero++;
+        // System.out.println(++ numero);
 
-        System.out.println(numero);
+        // System.out.println(numero);
+
+        // System.out.println( numero --);
+
+        // System.out.println(numero);
+
+        // boolean variavel = true;
+
+        // System.out.println(variavel);
+
+        // variavel = !variavel;
+
+        // System.out.println(variavel);
+
+        // int a, b;
+
+        // a = 5;
+        // b = 6;
+
+        //String resultado = "";
+        //    if(a==b)
+        //        resultado = "verdadeiro";
+        //    else
+        //        resultado = "falso";
+
+        // String resultado = a==b ? "verdadeiro" : "falso";
+
+        // int resultado = a==b ? 1 : 0;
+
+        // System.out.println(resultado);
+
+        // String nomeUm = "AluHnor";
+        // String nomeDois = new String("AluHnor");
+
+        // System.out.println(nomeUm == nomeDois);
+
+        // System.out.println(nomeUm.equals(nomeDois));
+
+        // int numero1 = 1;
+        // int numero2 = 2;
+
+        // boolean simNao = numero1 == numero2;
+
+        // if(numero1 < numero2) {
+        //     System.out.println("condição verdadeira");
+        // }
+
+        //System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        // simNao = numero1 != numero2;
+
+        // System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        // simNao = numero1 > numero2;
+
+        // System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+    boolean condicao1 = true;
+
+    boolean condicao2 = false; 
+
+    if (condicao1 && (6 > 2)) {
+        System.out.println("ambas veradadeiras");
+
+    }
+
+    if (condicao1 || condicao2) {
+        System.out.println("parcialmente verdadeira");
+
+    }
+
+
 
     }
 }
